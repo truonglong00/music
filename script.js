3aad8a5e27d2c7e03e05055ef9bd8ddc7d756a74
truongtieulong <tieulongtruong00@gmail.com>
2022-11-27 21:01:30 +0700

    hello

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var source = "https://github.com/truonglong00/music/raw/main/music/khongcanphaihuadauem.mp3"
- var audio = document.createElement("audio");
- //
- audio.autoplay = true;
- //
- audio.load()
- audio.addEventListener("load", function() { 
-     audio.play(); 
- }, true);
- audio.src = source;
+
+ window.onload = function() {
+  var audio = document.createElement("audio");
+  //
+  audio.autoplay = true;
+  //
+  audio.load()
+  audio.addEventListener("load", function() { 
+      audio.play(); 
+  }, true);
+  audio.src = source;
+}
